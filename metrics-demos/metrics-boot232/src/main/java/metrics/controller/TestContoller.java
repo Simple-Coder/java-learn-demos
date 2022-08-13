37900778d8a2d721c562e24241e637c2d7a9ec5b
xiedong <xiedong@xx.com>
2022-08-13 16:41:31 +0800

    boot 232

---
+++ metrics-demos/metrics-boot232/src/main/java/metrics/controller/TestContoller.java
@@ -0,0 +1,33 @@
+package metrics.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created by xiedong
+ * Date: 2022/7/24
+ */
+@RestController
+@Slf4j
+@RequestMapping("/metric1")
+public class TestContoller {
+    //  http:localhost:8989/metric1/a
+    // http://127.0.0.1:9998/metrics
+
+    @Resource
+    private ThreadPoolExecutor dtpExecutor1;
+
+    @GetMapping("/a")
+    public String a() {
+
+//        DtpExecutor dtpExecutor = DtpRegistry.getDtpExecutor("dtpExecutor1");
+//        dtpExecutor.execute(() -> System.out.println("test"));
+
+        return Thread.currentThread().getName();
+    }
+}
