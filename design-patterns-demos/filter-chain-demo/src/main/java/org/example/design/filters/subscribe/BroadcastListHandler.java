cf8207544a2905cf8a5bf88efa05e81af5cebfa1
xiedong <1412834290@qq.com>
2022-08-18 21:55:25 +0800

    handler增加

---
+++ design-patterns-demos/filter-chain-demo/src/main/java/org/example/design/filters/subscribe/BroadcastListHandler.java
@@ -0,0 +1,15 @@
+package org.example.design.filters.subscribe;
+
+import org.example.design.filters.abs.AbstractFeedHandler;
+import org.example.design.filters.context.SubscribeContext;
+
+/**
+ * Created by xiedong
+ * Date: 2022/8/18 21:38
+ */
+public class BroadcastListHandler extends AbstractFeedHandler<SubscribeContext> {
+    @Override
+    protected void handle(SubscribeContext ctx) {
+
+    }
+}
---
+++ design-patterns-demos/filter-chain-demo/src/main/java/org/example/design/filters/subscribe/LivePreviewHandler.java
@@ -0,0 +1,8 @@
+package org.example.design.filters.subscribe;
+
+/**
+ * Created by xiedong
+ * Date: 2022/8/18 21:38
+ */
+public class LivePreviewHandler {
+}
