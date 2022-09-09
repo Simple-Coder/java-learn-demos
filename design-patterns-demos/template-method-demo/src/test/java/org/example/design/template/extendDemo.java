79bef9c2a124c87c75d314f115b36f836b75c41a
dongxie <dongxie@sohu-inc.com>
2022-09-09 18:41:02 +0800

    多态测试

---
+++ design-patterns-demos/template-method-demo/src/test/java/org/example/design/template/extendDemo.java
@@ -0,0 +1,32 @@
+package org.example.design.template;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Created by dongxie on 2022/9/9.
+ */
+public class extendDemo {
+    public static void main(String[] args) {
+        Animal cat = new Cat();
+        cat.setAge(12);
+        cat.setName("xiao cat");
+//        cat.setSex(1);
+
+        System.out.println();
+    }
+}
+
+
+@Data
+class Animal {
+    protected String name;
+    protected int age;
+}
+@EqualsAndHashCode(callSuper = true)
+@Data
+class Cat extends Animal {
+    private String name;
+    private int age;
+    private int sex;
+}
