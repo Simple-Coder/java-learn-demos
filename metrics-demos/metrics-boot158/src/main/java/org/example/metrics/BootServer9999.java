e2a3b000b7911400e9dd92362bbe8b916cad69f0
dong <1412834290@qq.com>
2022-09-27 21:15:58 +0800

    测试双实例+eureka

--- metrics-demos/metrics-boot158/src/main/java/org/example/metrics/BootServer9999.java
+++ metrics-demos/metrics-boot158/src/main/java/org/example/metrics/BootServer9999.java
@@ -3,6 +3,7 @@ package org.example.metrics;
 //import com.sohu.hd.metrics.custom.annotation.EnableControllerCost;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
  */
 @SpringBootApplication
 @EnableHystrix
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableControllerCost
 public class BootServer9999 {
     public static void main(String[] args) {
