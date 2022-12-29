81a61db94494792a7b950861dd3548e2b11ee96d
dongxie <dongxie@sohu-inc.com>
2022-12-29 14:21:24 +0800

    并发测试

--- boot-demos/threadpool-demo/src/main/java/org/example/tp/TestSet.java
+++ boot-demos/threadpool-demo/src/main/java/org/example/tp/TestSet.java
@@ -1,7 +1,20 @@
 package org.example.tp;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ConcurrencyTester;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.ZipUtil;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.poi.excel.BigExcelWriter;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
 import com.alibaba.fastjson.JSONObject;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,18 +23,50 @@ import java.util.Set;
  */
 public class TestSet {
     public static void main(String[] args) {
-        Set<Long> a1= new HashSet<>();
-        a1.add(1L);
-        a1.add(2L);
-        a1.add(3L);
 
-        JSONObject jsonObject = new JSONObject();
+        int i = 1;
+        while (true) {
 
-        int status = jsonObject.getIntValue("status");
+            ConcurrencyTester tester = ThreadUtil.concurrencyTest(10, () -> {
+                // 测试的逻辑内容
+                long delay = RandomUtil.randomLong(1, 50);
+                HttpResponse execute = HttpUtil.createGet("http://localhost:9999/hystrix/test?num=" + delay).execute();
+                System.out.println(execute.body());
+//            long delay = RandomUtil.randomLong(100, 1000);
+//            ThreadUtil.sleep(delay);
+//            Console.log("{} test finished, delay: {}", Thread.currentThread().getName(), delay);
+            });
+
+            Console.log(tester.getInterval());
+            if (i++ >= 10000) {
+                break;
+            }
+        }
+// 获取总的执行时间，单位毫秒
 
 
-        Long a2 = 1L;
-        boolean contains = a1.contains(a2);
-        System.out.println();
+//        String result = "";
+//        for (int i = 12; i < 1200; i += 12) {
+//            String str = "D" + i;
+//            result += str;
+//            result += ",";
+//        }
+//        System.out.println(result);
+//        File zip = ZipUtil.zip("D:\\testxls","D:\\a.zip");
+
+
+//        Set<Long> a1= new HashSet<>();
+//        a1.add(1L);
+//        a1.add(2L);
+//        a1.add(3L);
+//
+//        JSONObject jsonObject = new JSONObject();
+//
+//        int status = jsonObject.getIntValue("status");
+//
+//
+//        Long a2 = 1L;
+//        boolean contains = a1.contains(a2);
+//        System.out.println();
     }
 }
