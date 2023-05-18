fe8fe1425be4997b58c19e5c7b0ac910a14ad9cc
xiedong <dxiesjz@163.com>
2023-05-18 22:08:04 +0800

    bio

--- test-demos/src/main/java/demos/test/io/bio/Client1.java
+++ test-demos/src/main/java/demos/test/io/bio/Client1.java
@@ -8,7 +8,7 @@ import java.net.Socket;
  * Created by xiedong
  * Date: 2023/5/18 22:01
  */
-public class BIOClient {
+public class Client1 {
     @SneakyThrows
     public static void main(String[] args) {
         Socket socket = new Socket("127.0.0.1", 8080);
