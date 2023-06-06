d444ed85db14e70c36a47c231c15069c83c0e61c
dongxie <dongxie@sohu-inc.com>
2023-06-06 13:07:09 +0800

    将链表的前 n 个节点反转（n <= 链表长度）

--- lbld-demos/src/main/java/leecode/demos/Code92.java
+++ lbld-demos/src/main/java/leecode/demos/Code92.java
@@ -12,7 +12,9 @@ public class Code92 {
     public static void main(String[] args) {
         Code92 code92 = new Code92();
         ListNode head = code92.buildListNode();
-        ListNode reverse = code92.reverse(head);
+//        ListNode reverse = code92.reverse(head);
+        // 将链表的前 n 个节点反转（n <= 链表长度）
+        ListNode node1 = code92.reverseN(head, 2);
         ListNode node = code92.reverseBetween(head, 2, 4);
         System.out.println();
     }
@@ -39,6 +41,26 @@ public class Code92 {
         return head;
     }
 
+
+    ListNode successor = null;//后驱节点
+
+    // 将链表的前 n 个节点反转（n <= 链表长度）
+    ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            // 记录第 n + 1 个节点
+            successor = head.next;
+            return head;
+        }
+
+        // 以 head.next 为起点，需要反转前 n - 1 个节点
+        ListNode last = reverseN(head.next, n - 1);
+        //反转
+        head.next.next = head;
+        // 让反转之后的 head 节点和后面的节点连起来
+        head.next = successor;
+
+        return last;
+    }
 
     // 定义：输入一个单链表头结点，将该链表反转，返回新的头结点
     ListNode reverse(ListNode head) {
